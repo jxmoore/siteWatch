c5bc6d3ac0448d1a1e4109d434bb35cb4f596d55
jxmoore <joe.moore1988@outlook.com>
2019-08-25 22:37:44 -0400

    more added to notifications, removed old println's

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -2,8 +2,11 @@
 package controllers
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"strings"
 	"sync"
 	"time"
@@ -65,9 +68,7 @@ func siteCheck(siteList *models.SiteBlock, index int, wg *sync.WaitGroup) {
 	} else if testSite.StatusCode != site.Result {
 		site.Count++
 		site.Status = false
-		// fmt.Printf("Error test %v : %v != %v \n", site.Address, testSite.StatusCode, site.Result)
 	} else {
-		// fmt.Printf("Test %s : %d \n", site.Address, testSite.StatusCode)
 		if !site.Status {
 			site.Status = true
 		}
@@ -82,9 +83,23 @@ func siteCheck(siteList *models.SiteBlock, index int, wg *sync.WaitGroup) {
 }
 
 // notify is responsible for notifying when failures exceed the threshold.
-func notify(siteName string, count int, notifier *models.Notification) {
+func notify(siteName string, count int, notifier *models.Notification) error {
 	notifier.Kind = strings.ToLower(notifier.Kind)
 	if notifier.Kind == "" || strings.Contains(notifier.Kind, "stdout") {
 		fmt.Printf("The test for %s has failed %d times which exceeds the current threshold value.\n", siteName, count)
+	} else if notifier.Kind == "log" {
+		if notifier.Path == "" {
+			notifier.Path = "./siteWatch.log"
+		}
+		logFile, err := os.OpenFile(notifier.Path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			return errors.New("Error opening log file")
+		}
+		defer logFile.Close()
+		logger := log.New(logFile, "", log.LstdFlags)
+		logger.Printf("The test for %s has failed %d times which exceeds the current threshold value.\n", siteName, count)
+	} else {
+		fmt.Printf("The test for %s has failed %d times which exceeds the current threshold value.\n", siteName, count)
 	}
+	return nil
 }
