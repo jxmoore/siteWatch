9a7eb51631ac26642dbe0720df1f5f4fb25b6cf2
Jxmoore <Joe.Moore1988@outlook.com>
2019-10-05 20:58:17 -0400

    small changes

--- controllers/notification.go
+++ controllers/notification.go
@@ -10,8 +10,9 @@ import (
 	"github.com/jxmoore/siteWatch/models"
 )
 
-// notify is responsible for notifying when failures exceed the threshold.
+// Notify is responsible for notifying when failures exceed the threshold.
 func notify(siteName string, count int, notifier *models.Notification) error {
+
 	notifier.Kind = strings.ToLower(notifier.Kind)
 	if notifier.Kind == "" || strings.Contains(notifier.Kind, "stdout") {
 		fmt.Printf("The test for %s has failed %d times which exceeds the current threshold value.\n", siteName, count)
@@ -19,15 +20,20 @@ func notify(siteName string, count int, notifier *models.Notification) error {
 		if notifier.Path == "" {
 			notifier.Path = "./siteWatch.log"
 		}
+
 		logFile, err := os.OpenFile(notifier.Path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 		if err != nil {
 			return errors.New("Error opening log file")
 		}
+
 		defer logFile.Close()
+
 		logger := log.New(logFile, "", log.LstdFlags)
 		logger.Printf("The test for %s has failed %d times which exceeds the current threshold value.\n", siteName, count)
+
 	} else {
 		fmt.Printf("The test for %s has failed %d times which exceeds the current threshold value.\n", siteName, count)
 	}
+
 	return nil
 }
